7e396246d526d650d6405086996b3aeff33acd96
Yvan Valens <nsengvalens4@gmail.com>
2020-11-13 18:05:52 +0200

    feat(login):User login

--- redux/reducers/auth.js
+++ redux/reducers/auth.js
@@ -1,22 +1,35 @@
-import { SET_ERROR, SET_AUTH_STATUS } from "../actions/types";
+import { SET_ERROR, SET_LOADING, SET_RESPONSE } from "../actions/types";
 
 const initialState = {
+  loading: null,
   error: null,
-  isAuthenticated: null,
+  response: null,
+  isAuthenticated: false,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
     case SET_ERROR:
       return {
         ...state,
+        isAuthenticated: false,
         error: action.payload,
+        loading: null,
       };
-    case SET_AUTH_STATUS:
+
+    case SET_RESPONSE:
       return {
         ...state,
         isAuthenticated: true,
+        response: action.payload,
+        loading: null,
       };
+
     default:
       return state;
   }
